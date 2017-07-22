d343502a046c196e83b7ebec837dd6dd9c51eaf7
zhaoyao91 <zhaoyao91@163.com>
2017-07-23 00:07:25 +0800

    remove old code

--- old/lib/hohs/with_serializer.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (serializer) {
-  return function (handler) {
-    return async (input, subject) => {
-      input = serializer.deserialize(input)
-      const output = await handler(input, subject)
-      return serializer.serialize(output)
-    }
-  }
-}
--- old/lib/serializers/json.js
+++
@@ -1,4 +0,0 @@
-module.exports = options => ({
-  serialize: (data) => JSON.stringify(data),
-  deserialize: (data) => data === undefined ? undefined : JSON.parse(data),
-})
